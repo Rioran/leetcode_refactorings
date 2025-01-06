a477441bca1b7bc2c13417a2cfdef7539c783063
Rioran <blackdragon71325@gmail.com>
2025-01-06 18:37:07 +0400

    docs: add README content and initial script for refactoring

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # leetcode_refactorings
 A project to try out some refactoring techniques inspired by Martin Fowler book.
+
+Script for refactoring must be added to the main branch. Specific refactorings, though, must be applyed in a different branch, dedicated to this specific script.
+
+Refactorings intent to be as small as possible. Test phase depends on leetcode itself and is out of scope for this specific project.
---
+++ scripts/rearrange_string_with_char_distance.py
@@ -0,0 +1,33 @@
+'''
+Leetcode task:
+
+Given a string s and an integer k,
+rearrange s such that the same characters are at least distance k from each other.
+If it is not possible to rearrange the string, return an empty string "".
+'''
+import collections
+
+
+class Solution:
+    def rearrangeString(self, s: str, k: int) -> str:
+        counter = collections.Counter(s)
+        items = sorted([(freq, ch) for ch, freq in counter.items()])
+        maxFreq = items[-1][0]
+        slots = ["" for _ in range(maxFreq)]
+
+        slot = 0
+        while (items):
+            freq, ch = items.pop()
+            if (freq == maxFreq):
+                for i in range(maxFreq):
+                    slots[i] = slots[i] + ch
+            else:
+                while (freq):
+                    slot = slot % (maxFreq - 1)
+                    slots[slot] = slots[slot] + ch
+                    freq -= 1
+                    slot += 1
+        for i in range(maxFreq - 1):  # up until last slot
+            if len(slots[i]) < k:
+                return ""
+        return "".join(slots)
