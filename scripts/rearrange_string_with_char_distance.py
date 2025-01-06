336ef23d3e59375b34e2495e182b770c0a4d05a1
Rioran <blackdragon71325@gmail.com>
2025-01-06 19:06:12 +0400

    refactor: rename variable maxFreq -> top_occurrence

--- scripts/rearrange_string_with_char_distance.py
+++ scripts/rearrange_string_with_char_distance.py
@@ -16,22 +16,22 @@ class Solution:
     def rearrangeString(self, text: str, char_distance: int) -> str:
         counter = Counter(text)
         increasing_char_occurences = get_increasing_char_occurences(counter)
-        maxFreq = increasing_char_occurences[-1][0]
-        slots = ["" for _ in range(maxFreq)]
+        top_occurrence = increasing_char_occurences[-1][0]
+        slots = ["" for _ in range(top_occurrence)]
 
         slot = 0
         while (increasing_char_occurences):
             freq, ch = increasing_char_occurences.pop()
-            if (freq == maxFreq):
-                for i in range(maxFreq):
+            if (freq == top_occurrence):
+                for i in range(top_occurrence):
                     slots[i] = slots[i] + ch
             else:
                 while (freq):
-                    slot = slot % (maxFreq - 1)
+                    slot = slot % (top_occurrence - 1)
                     slots[slot] = slots[slot] + ch
                     freq -= 1
                     slot += 1
-        for i in range(maxFreq - 1):  # up until last slot
+        for i in range(top_occurrence - 1):  # up until last slot
             if len(slots[i]) < char_distance:
                 return ""
         return "".join(slots)
