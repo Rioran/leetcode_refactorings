4384dc32680f8e2c62bb28f5cb071a4875c2c6af
Rioran <blackdragon71325@gmail.com>
2025-01-07 00:19:16 +0400

    refactor: change function name get_decreasing_char_occurences -> get_decreasing_char_occurrences due typo

--- scripts/rearrange_string_with_char_distance.py
+++ scripts/rearrange_string_with_char_distance.py
@@ -27,7 +27,7 @@ class CharSlots:
             self.add_char_to_active_slot(char)
 
 
-def get_decreasing_char_occurences(text: str) -> list[tuple[int, str]]:
+def get_decreasing_char_occurrences(text: str) -> list[tuple[int, str]]:
     return list(map(lambda x: x[::-1], Counter(text).most_common()))
 
 
@@ -37,7 +37,7 @@ def detect_slots_overflow(char_slots, char_distance):
 
 class Solution:
     def rearrangeString(self, text: str, char_distance: int) -> str:
-        decreasing_char_occurences = get_decreasing_char_occurences(text)
+        decreasing_char_occurences = get_decreasing_char_occurrences(text)
         char_slots = CharSlots(slots_count=decreasing_char_occurences[0][0])
 
         for occurrences, char in decreasing_char_occurences:
